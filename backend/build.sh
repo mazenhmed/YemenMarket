d8067c44967afd36733d1221652bf8c6f0e20406
abo <abomalk1670@gmail.com>
2026-04-15 19:44:53 +0300

    Automate superuser creation on deployment

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ -f "datadump.json" ]; then
+    echo "Migrating old data to PostgreSQL..."
+    python manage.py loaddata datadump.json
+fi
+
 python manage.py shell -c "from users.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@yemenmarket.com', 'admin123')"
