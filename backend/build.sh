e22f91195d4214a64488e0e8104238e736c20b70
abo <abomalk1670@gmail.com>
2026-04-15 19:42:49 +0300

    Automate superuser creation on deployment

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from users.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@yemenmarket.com', 'admin123')"
